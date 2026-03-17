6f3bbb9ab98629f8d6a5004d81d0ed6889a18e90
MaxB3l <belka2014.max@gmail.com>
2026-03-17 22:31:14 +0700

    обавлена Google reCAPTCHA v2

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from wtforms.validators import DataRequired
 import io
 import os
 import base64
+import requests
 import random
 import string
 import numpy as np
@@ -16,6 +17,8 @@ from PIL import Image, ImageDraw
 app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), 'templates'))
 app.config['SECRET_KEY'] = '12345'
 #app.config['UPLOAD_FOLDER'] = 'static/uploads'
+app.config['RECAPTCHA_PUBLIC_KEY'] = '6LfrkY0sAAAAALVCa27xBLHxQ8qzSzoSFdNwG3od'
+app.config['RECAPTCHA_PRIVATE_KEY'] = 'Т6LfrkY0sAAAAAC8s61zINrelmAoGkZon1Aof3LKY'
 
 class ImageForm(FlaskForm):
     """Форма для обработки изображения"""
@@ -25,6 +28,22 @@ class ImageForm(FlaskForm):
                                    ('horizontal', 'Горизонтальный')],
                            default='vertical')
     color = StringField('Цвет', default='#ff0000')
+
+
+def verify_recaptcha(response_token):
+    """Проверяет токен reCAPTCHA"""
+    secret_key = app.config['RECAPTCHA_PRIVATE_KEY']
+    verify_url = 'https://www.google.com/recaptcha/api/siteverify'
+
+    data = {
+        'secret': secret_key,
+        'response': response_token
+    }
+
+    result = requests.post(verify_url, data=data)
+    response_json = result.json()
+
+    return response_json.get('success', False)
 
 def get_color_histogram(img):
     """Создаёт гистограмму цветов изображения"""
@@ -56,11 +75,21 @@ def index():
 
     form = ImageForm()
 
-    return render_template('index.html', form=form)
+    return render_template('index.html',
+                          form=form,
+                          recaptcha_public_key=app.config['RECAPTCHA_PUBLIC_KEY'])
 
 @app.route('/process', methods=['POST'])
 def process():
     from PIL import Image, ImageDraw
+
+    recaptcha_response = request.form.get('g-recaptcha-response')
+
+    if not recaptcha_response:
+        return jsonify({'error': 'Подтвердите, что вы не робот!'}), 400
+
+    if not verify_recaptcha(recaptcha_response):
+        return jsonify({'error': 'Неверная капча! Попробуйте ещё раз.'}), 400
 
     file = request.files.get('image')
     cross_type = request.form.get('cross_type')
